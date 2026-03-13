1cef1438bdfdf6f2886912c21d0458ae524bb691
Manideep-07 <83880837+Manideep-07@users.noreply.github.com>
2026-03-13 15:01:02 +0530

    Delete project_github_upload_ready_topics directory

--- project_github_upload_ready_topics/tests/run_tests.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-with open('test_clean.txt', 'w', encoding='utf-8') as f:
-    result = subprocess.run(['.venv\\\\Scripts\\\\python', 'tests/system_test.py'], capture_output=True, text=True)
-    f.write(result.stdout)
-    if result.stderr:
-        f.write("\nSTDERR:\n")
-        f.write(result.stderr)
